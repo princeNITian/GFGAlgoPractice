247b871eb77357e80bcaa0eda17151aec59b0d04
Prince Raj <princeraj427@gmail.com>
2020-09-09 00:52:05 +0530

    Ugly number added

--- DP/1.py
+++ DP/1.py
@@ -1,1 +1,31 @@
-print('Hello')
+def maxDivide(a,b):
+    while a%b == 0:
+        a = a / b
+        
+    return a
+    
+def isUgly(no):
+    no = maxDivide(no,2)
+    no = maxDivide(no,3)
+    no = maxDivide(no,5)
+    return 1 if no == 1 else 0
+    
+def getNthUgly(n):
+    i = 1
+    count = 1
+    while n>count:
+        i += 1
+        if isUgly(i):
+            count += 1
+            
+    return i
+    
+
+
+
+
+if __name__=="__main__":
+    no = getNthUgly(int(input()))
+    print('Ugly no: ',no)
+    
+    
--- DP/README.md
+++ DP/README.md
@@ -5,6 +5,13 @@
     >- Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, … shows the first 11 ugly numbers. By convention, 1 is included.
 
     >- Given a number n, the task is to find n’th Ugly number.
+
+    >Examples:<br>
+    Input  : n = 7
+    Output : 8<br>
+    Input  : n = 10
+    Output : 12
+
     
 - [Fibonacci Numbers](2.py)
 
